454c00eb0c60343bb384c1fb4f5bcae8cd1b4e03
Cxetive <54766299+Cxetive@users.noreply.github.com>
2022-07-08 17:25:27 +0100

    Update tappingsim.lua
    fixed var edit

--- tappingsim.lua
+++ tappingsim.lua
@@ -1,6 +1,12 @@
 repeat wait() until game:IsLoaded()
 
-local Folder = Instance:n
+local Folder = Instance.new("Folder", game.replicatedstore)
+Folder.name = "Prox"
+
+game.players.localplayer.character:MoveTo(37.2773, 9.55834, -581.942)
+
+local clone = game.Workspace.Proximity.Enchantment:Clone()
+clone.Parent = game.ReplicatedStorage.Prox
 
 local UI = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
 
